f278a8691ef3c2c3dcc1e65ecea966f07dd13d8f
Robert Lane <dev@carno.io>
2023-08-25 03:25:32 +0100

    issue that prevented install

--- src/Models/ConversationMessage.php
+++ src/Models/ConversationMessage.php
@@ -14,6 +14,8 @@ class ConversationMessage extends \Illuminate\Database\Eloquent\Model
     use HasOrderedUUID,
         SoftDeletes;
 
+    protected $guarded = [];
+
     public function conversation(){
         return $this->belongsTo(Conversation::class);
     }
